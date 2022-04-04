6c23de944a47f387709459e706bed0e040945241
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-04-04 10:55:55 -0700

    wrote oddishOrEvenish

--- index.js
+++ index.js
@@ -11,5 +11,21 @@ function reverseWords(sentence) {
   return words;
 }
 
+const num1 = 121;
+const num2 = 41;
 
-console.log(reverseWords(sentence));
+function oddishOrEvenish(num) {
+  let num1 = num.toString().split('');
+
+  let sum = 0;
+
+  for (let i = 0; i < num1.length; i++) {
+    sum +=  Number(num1[i]);
+  }
+  if (sum % 2 === 0) {
+    return 'Evenish';
+  } else {
+    return 'Oddish';
+  }
+}
+console.log(oddishOrEvenish(num2));
