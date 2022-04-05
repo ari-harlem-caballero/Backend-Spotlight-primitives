d0bdba5fa473d87216dbe528d28e344e166e7889
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-04-05 10:45:20 -0700

    wrote .pop()

--- queue/index.js
+++ queue/index.js
@@ -7,9 +7,16 @@ class Stack {
     return this.stack.push(item);
   }
 
+  pop() {
+    return this.stack.pop();
+  }
+
 }
 
 const newStack = new Stack();
 newStack.push('fox');
+newStack.push('goose');
+newStack.push('lizard');
+newStack.pop();
 
 console.log(newStack);
